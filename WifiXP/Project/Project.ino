77394dadb7238e617b6336ae96e37306b4a8ea0c
Marcin Nowiński <marcin.nowinski25@gmail.com>
2019-09-13 03:38:30 +0200

    Project Arduino reworked

--- WifiXP/Project/Project.ino
+++ WifiXP/Project/Project.ino
@@ -6,8 +6,6 @@
 
 #include "class/Logger.cpp"
 #include "class/Connector.cpp"
-#include "class/IConnect.cpp"
-#include "class/IPSplitter.cpp"
 
 ESP8266WebServer server(80);
 
@@ -16,11 +14,17 @@ Connector connector;
 
 void handleRoot() {
     String s;
+    bool ret;
     logger.clearLogs();
     int numberOfNetworks = WiFi.scanNetworks();
     logger.startLogging(numberOfNetworks);
         for (int i = 0; i < numberOfNetworks; i++) {
-          bool ret = false;
+          String password = String(WiFi.encryptionType(i));
+          if (password == "7") {
+            ret = true;
+          } else {
+            ret = false;
+          }
           String rssi = String(WiFi.RSSI(i));
           logger.addNewWifi(WiFi.SSID(i).c_str(), rssi.c_str(), ret);
         }
@@ -28,6 +32,21 @@ void handleRoot() {
     s = logger.getLogs();
     server.send(200, "text/html", logger.prepareResponse((const char *) s.c_str()));
     logger.clearLogs();
+}
+
+
+void handlePing() {
+  String res;
+  bool connection = connector.ping();
+  Serial.println(connection);
+  if(connection == true) {
+    res = "WiFi connected to Internet";
+  }
+  else {
+    res = "WiFi not connected to Internet";
+  }
+  
+  server.send(200, "text/html", res.c_str());
 }
 
 
@@ -54,6 +73,7 @@ void setup() {
     Serial.print("AP IP address: ");
     Serial.println(myIP);
     server.on("/", handleRoot);
+    server.on("/ping", handlePing);
     server.begin();
     Serial.println("HTTP server started");
 }
@@ -61,7 +81,7 @@ void setup() {
 void loop() {
     server.handleClient();
     for (int i = 0; i < 10; i++) {
-        delay(1000);
+        delay(100);
         server.handleClient();
     }
 }
